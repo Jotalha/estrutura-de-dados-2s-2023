e45e82201eb1bfa3119858c89be97f532ea4dc4b
joaopxt <peixoto.jvs@gmail.com>
2023-10-05 16:03:08 -0300

    Bubble sort corrigido

--- atividade5/main.c
+++ atividade5/main.c
@@ -1,47 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bubble_sort.h"
-
-void imprimir_arr(int arr[], int n)
-{
-    printf("\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-
     FILE *fp;
-
     fp = fopen(argv[1], "r");
 
-    int quant = 0;
-
-    char txt[100];
-    while (!feof(fp))
+    if (fp == NULL)
     {
-        fgets(txt, 100, fp);
-        printf("%s\n", txt);
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    int quant = 0;
+    char txt[100];
+    char palavras[100][100];
+
+    while (fgets(txt, 100, fp) != NULL)
+    {
+        printf("%s", txt);
+        strcpy(palavras[quant], txt);
         quant++;
     }
 
-    bubble_sort(txt, quant);
-
     fclose(fp);
 
-    fp = fopen(argv[1], "r");
+    bubble_sort(palavras, quant);
 
-    while (!feof(fp))
+    FILE *f;
+    f = fopen("arq_palavras_ordenado.txt", "w");
+
+    if (f == NULL)
     {
-        fgets(txt, 100, fp);
-        printf("%s\n", txt);
+        perror("Erro ao criar o arquivo de saída");
+        return 1;
     }
 
-    fclose(fp);
+    for (int i = 0; i < quant; i++)
+    {
+        fprintf(f, "%s", palavras[i]);
+    }
 
-    exit(0);
+    fclose(f);
+
+    return 0;
+
+    /*   FILE *fp;
+
+       fp = fopen(argv[1], "r");
+
+       int quant = 0;
+
+       char txt[100];
+       while (!feof(fp))
+       {
+           fgets(txt, 100, fp);
+           printf("%s\n", txt);
+           quant++;
+       }
+
+       bubble_sort(txt, quant);
+
+       FILE *f;
+
+       f = fopen("arq_palavras_ordenado.txt", "w");
+       if (f == NULL)
+       {
+           perror("Nao foi possivel abrir o arquivo");
+           exit(1);
+       }
+
+       fscanf(fp, "%s", txt);
+
+       fclose(fp);
+
+       fclose(f);
+
+       exit(0);*/
 }
